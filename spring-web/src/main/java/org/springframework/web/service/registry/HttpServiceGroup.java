0e23804ef305623e152e1326cc763c1defb87f3b
rstoyanchev <rossen.stoyanchev@broadcom.com>
2025-02-18 11:56:53 +0000

    Add HttpServiceProxyRegistry

---
+++ spring-web/src/main/java/org/springframework/web/service/registry/HttpServiceGroup.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright 2002-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.web.service.registry;
+
+import java.lang.annotation.Annotation;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Consumer;
+
+import org.jspecify.annotations.Nullable;
+
+import org.springframework.core.type.filter.TypeFilter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+/**
+ * A grouping of HTTP service types that share a client with the same setup.
+ *
+ * @author Rossen Stoyanchev
+ * @since 7.0
+ * @param <CB> the type of client builder (e.g. RestClient.Builder)
+ */
+public interface HttpServiceGroup<G extends HttpServiceGroup<G, CB>, CB> {
+
+	/**
+	 * Return the base URL for the HTTP Service group.
+	 */
+	String baseUrl();
+
+	/**
+	 * Return the name for the HTTP Service group, or {@code null} if not configured.
+	 */
+	@Nullable String name();
+
+	/**
+	 * Return the configured HTTP Service types.
+	 */
+	Set<Class<?>> httpServiceTypes();
+
+	/**
+	 * Add the given HTTP service types.
+	 */
+	G addHttpServiceTypes(Class<?>... httpServiceTypes);
+
+	/**
+	 * Scan the classpath for HTTP Service types under one or more base packages,
+	 * and with a list of include and exclude filters.
+	 * <p>By default, if no include filters are specified, then a filter is added
+	 * to search for interfaces annotated with
+	 * {@link org.springframework.web.service.annotation.HttpExchange}.
+	 * @see org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider
+	 */
+	G detectHttpServiceTypes(Consumer<ScanSpec> scanConfigurer);
+
+	/**
+	 * Callback to configure the underlying HTTP client.
+	 */
+	G configureClient(Consumer<CB> configurer);
+
+	/**
+	 * Callback to configure the {@link HttpServiceProxyFactory} used
+	 * to create proxy instances.
+	 */
+	G configureProxyFactory(Consumer<HttpServiceProxyFactory.Builder> configurer);
+
+	/**
+	 * Create proxy instances for all HTTP Service types.
+	 */
+	void initProxies();
+
+	/**
+	 * Return the proxy instance for the given HttpService type.
+	 * @param <T> the proxy type
+	 * @throws IllegalArgumentException if there is no proxy of the given type,
+	 * or the {@code HttpServiceGroup} hasn't been initialized.
+	 */
+	<T> T getProxy(Class<T> proxyType);
+
+	/**
+	 * Return a Map from HttpService types to proxy instances. The returned Map is
+	 * empty before {@link #initProxies()} is called.
+	 */
+	Map<Class<?>, Object> getProxies();
+
+
+	/**
+	 * Spec to specify HTTP service scan options.
+	 */
+	interface ScanSpec {
+
+		/**
+		 * Add base packages to scan.
+		 */
+		ScanSpec basePackages(String... basePackages);
+
+		/**
+		 * Add base packages through references to classes in those packages.
+		 */
+		ScanSpec basePackages(Class<?>... basePackageClasses);
+
+		/**
+		 * Match HTTP Service types that are assignable to a given type.
+		 * @see org.springframework.core.type.filter.AssignableTypeFilter
+		 */
+		ScanSpec assignableTypes(Class<?>... assignableTypes);
+
+		/**
+		 * Match HTTP Service types that have a given annotation, checking inherited
+		 * annotations as well.
+		 * @see org.springframework.core.type.filter.AnnotationTypeFilter
+		 */
+		ScanSpec annotation(Class<? extends Annotation> annotation);
+
+		/**
+		 * Match HTTP Services types with a regex for fully-qualified class names.
+		 * @see org.springframework.core.type.filter.RegexPatternTypeFilter
+		 */
+		ScanSpec regex(String... expressions);
+
+		/**
+		 * Add filters for types that should not be matched.
+		 */
+		ScanSpec excludeFilters(TypeFilter... typeFilters);
+	}
+
+}
---
+++ spring-webflux/src/main/java/org/springframework/web/reactive/function/client/support/WebClientHttpServiceProxyRegistry.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2002-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.web.reactive.function.client.support;
+
+import org.jspecify.annotations.Nullable;
+
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.service.registry.AbstractHttpServiceProxyRegistry;
+
+/**
+ * @author Rossen Stoyanchev
+ * @since 7.0
+ */
+public final class WebClientHttpServiceProxyRegistry extends AbstractHttpServiceProxyRegistry<WebClientHttpServiceGroup> {
+
+	private final WebClient.Builder baseClientBuilder;
+
+
+	private WebClientHttpServiceProxyRegistry(WebClient.Builder baseClientBuilder) {
+		this.baseClientBuilder = baseClientBuilder;
+	}
+
+
+	@Override
+	protected WebClientHttpServiceGroup createGroup(
+			String baseUrl, @Nullable String name, ClassPathScanningCandidateComponentProvider componentProvider) {
+
+		WebClient.Builder builder = this.baseClientBuilder.clone();
+		return new WebClientHttpServiceGroup(baseUrl, name, builder, componentProvider);
+	}
+
+
+	public static WebClientHttpServiceProxyRegistry create(WebClient.Builder baseClientBuilder) {
+		return new WebClientHttpServiceProxyRegistry(baseClientBuilder);
+	}
+
+}
